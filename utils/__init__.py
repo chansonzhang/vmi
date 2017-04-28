5bc3a7a09f94fe96282972b45da43dd3723c8d8f
Zhang Chen <zhangchen.shaanxi@gmail.com>
2017-04-28 22:09:06 +0800

    add code lines to record introspection latency

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# @Time    : 2017/4/28 21:49
+# @Author  : Zhang Chen
+# @Email    : zhangchen.shaanxi@gmail.com
+# @File    : __init__.py.py
---
+++ utils/fileUtils.py
@@ -0,0 +1,4 @@
+# @Time    : 2017/4/28 21:22
+# @Author  : Zhang Chen
+# @Email    : zhangchen.shaanxi@gmail.com
+# @File    : fileUtils.py
