a0158d187d456ecde02d331895b9627734847c0b
Zhang Chen <zhangchen.shaanxi@gmail.com>
2017-04-29 09:03:33 +0800

    modify testFileUtils.py

--- test/testFileUitls.py
+++ test/testFileUitls.py
@@ -2,3 +2,15 @@
 # @Author  : Zhang Chen
 # @Email    : zhangchen.shaanxi@gmail.com
 # @File    : testFileUitls.py.py
+from ceilometer.compute.vmi.utils.fileUtils import FileUtils;
+import datetime;
+import time;
+
+def testFileUtils():
+    time1 = datetime.datetime.now();
+    time.sleep(1);
+    time2 = datetime.datetime.now();
+    latency = (time2 - time1).seconds;
+    FileUtils.write_introspection_latency(latency);
+
+testFileUtils()
