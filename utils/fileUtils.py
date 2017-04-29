4136858060f740f9b7142e1ff6b0a8a3d540435c
Zhang Chen <zhangchen.shaanxi@gmail.com>
2017-04-29 20:48:35 +0800

    modify fileUtils.py solve time stale problem

--- utils/fileUtils.py
+++ utils/fileUtils.py
@@ -9,18 +9,18 @@ LOG = log.getLogger(__name__)
 
 class FileUtils:
     data_dir = "/experiment/"
-    file_prefix = time.strftime("%Y%m%d%H0000-", time.localtime())
-    file_name = "introspection_latency";
 
     @classmethod
     def write_introspection_latency(cls,content):
+        file_prefix = time.strftime("%Y%m%d%H0000-", time.localtime())
+        file_name = "introspection_latency";
         if os.path.isdir(cls.data_dir):
             pass;
         else:
             os.mkdir(cls.data_dir,0o777);
 
         LOG.debug('Using dir %s', os.path.abspath(cls.data_dir))
-        file = open(cls.data_dir + cls.file_prefix + cls.file_name,'a');
+        file = open(cls.data_dir + file_prefix + file_name,'a');
         file.write(str(content)+"\n")
         file.close();
 
